5f1366c5a00b3439584516f8dfa5d4669e524cff
tonykrapatony <tonykrapatony@gmail.com>
2023-07-07 11:51:04 +0300

    added react blank

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt exercitationem eius, quas aperiam tempora quo ducimus quibusdam illum blanditiis numquam earum dolores voluptate optio magnam excepturi ullam molestiae omnis assumenda.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt exercitationem eius, quas aperiam tempora quo ducimus quibusdam illum blanditiis numquam earum dolores voluptate optio magnam excepturi ullam molestiae omnis assumenda. Incidunt exercitationem eius, quas aperiam tempora quo ducimus quibusdam illum blanditiis numquam earum dolores voluptate optio magnam excepturi ullam molestiae omnis assumenda.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </div>
+  )
+}
---
+++ src/pages/Shop.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Shop() {
+  return (
+    <div>
+        <h1>The store page</h1>
+    </div>
+  )
+}
