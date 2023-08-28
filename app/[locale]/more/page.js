92a43b4f3b67d386ee0f4981935ccd67753ef658
MiroslavKop <kopytovmiroslav@gmail.com>
2023-08-28 20:03:34 +0300

    add navbar

---
+++ app/[locale]/more/page.js
@@ -0,0 +1,9 @@
+import MainContainer from '@/components/MainContainer';
+
+export default function More() {
+  return (
+    <MainContainer>
+      <h1>More</h1>
+    </MainContainer>
+  );
+}
---
+++ app/[locale]/profile/page.js
@@ -0,0 +1,9 @@
+import MainContainer from '@/components/MainContainer';
+
+export default function Profile() {
+  return (
+    <MainContainer>
+      <h1>User Profile</h1>
+    </MainContainer>
+  );
+}
