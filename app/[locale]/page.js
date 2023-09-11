6e06649db93ed4a9bfd40d47733fdd494fa421cc
MiroslavKop <kopytovmiroslav@gmail.com>
2023-09-12 00:49:39 +0300

    add reviews for homepage

--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -1,10 +1,12 @@
 import MainContainer from '@/components/MainContainer/MainContainer';
 import BestPrice from '@/components/Pages/HomePage/BestPrice/BestPrice';
+import CustomerReviews from '@/components/Pages/HomePage/CustomerReviews/CustomerReviews';
 
 export default function Home() {
   return (
     <MainContainer keywords={'main page'}>
       <BestPrice />
+      <CustomerReviews />
     </MainContainer>
   );
 }
--- redux/store.js
+++ redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice';
 import { productsApi } from './productsAPI';
+import { reviewsApi } from './reviewsApi';
 import { userApi } from './userApi';
 
 export const store = configureStore({
@@ -8,6 +9,8 @@ export const store = configureStore({
     auth: authReducer,
     [userApi.reducerPath]: userApi.reducer,
     [productsApi.reducerPath]: productsApi.reducer,
+    [reviewsApi.reducerPath]: reviewsApi.reducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(userApi.middleware, productsApi.middleware),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(userApi.middleware, productsApi.middleware, reviewsApi.middleware),
 });
