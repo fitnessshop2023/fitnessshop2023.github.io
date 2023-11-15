9a12d6d9510c9da7c28eb16bd8b49c7af964f5b5
MiroslavKop <kopytovmiroslav@gmail.com>
2023-11-15 20:57:02 +0200

    fix bugs and fix blog section

---
+++ app/[locale]/blogs/page.js
@@ -0,0 +1,9 @@
+import MainContainer from '@/components/MainContainer/MainContainer';
+
+export default function Blogs() {
+  return (
+    <MainContainer>
+      <h1>Blogs</h1>
+    </MainContainer>
+  );
+}
